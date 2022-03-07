e7cc900a4a65fc326d0900fb9f24ec809c36adbe
Ryan-Everett <ryanjameseverett@googlemail.com>
2022-03-07 20:36:59 +0000

    Adding small comments

--- utilities/GenStackMaps.java
+++ utilities/GenStackMaps.java
@@ -9,12 +9,14 @@ public class GenStackMaps {
     public static void main(String[] args) throws IOException{
         genStackMapsInClass(args[0]);
     }
+
+    //Takes a class, generates stack maps where necessary and then re-writes to the same name
     static void genStackMapsInClass(String pathName) throws IOException {
-        byte[] bytecode = Files.readAllBytes(Paths.get(pathName)); // result of your instrumentation
+        byte[] bytecode = Files.readAllBytes(Paths.get(pathName));
         ClassReader cr = new ClassReader(bytecode);
         ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
         cr.accept(cw, ClassReader.SKIP_FRAMES);
-        bytecode = cw.toByteArray(); // with recalculated stack maps
+        bytecode = cw.toByteArray(); // Rewrite class with StackMaps
         Files.write(Paths.get(pathName), bytecode);
     }
 }
